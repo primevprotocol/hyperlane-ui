05f13c41b41a7208984a285baa920131b7973710
Shawn <44221603+shaspitz@users.noreply.github.com>
2023-11-13 23:15:17 -0800

    rm unneeded config

--- src/consts/tokens.ts
+++ src/consts/tokens.ts
@@ -1,17 +1,7 @@
 import { WarpTokenConfig } from '../features/tokens/types';
 
 export const tokenList: WarpTokenConfig = [
-  // Example collateral token for an EVM chain
-  {
-    type: 'collateral',
-    chainId: 17864,
-    address: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6', // hopefull this is the default
-    hypCollateralAddress: '0x40bA2B4F9cCC1fbb3f27c4D7d7449439187e6A34',
-    name: 'commitEther',
-    symbol: 'cETH',
-    decimals: 18,
-    logoURI: '/logos/weth.png', // See public/logos/
-  },
+  // Config copied from warp-ui-token-list.json in deploy artifacts
   {
     chainId: 5,
     name: "Ether",
